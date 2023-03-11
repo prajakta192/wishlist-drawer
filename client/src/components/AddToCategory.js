fff25f2873b2e3860bc9bce701e5942e36c069ad
prajakta192 <bujare.prajakta17@gmail.com>
2023-03-11 17:26:55 +0530

    added save Category & edit category functionality

--- client/src/components/AddToCategory.js
+++ client/src/components/AddToCategory.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import { Button, Col, Dropdown, Form, Row, Toast, ToastContainer } from "react-bootstrap";
+import { Button, Col, Dropdown, Row, Toast, ToastContainer } from "react-bootstrap";
 
-const AddToCategory = ({ isLoggedIn, initialvalue, wishlist, closeCart }) => {
+  const AddToCategory = ({ isLoggedIn, initialvalue, wishlist, closeCart }) => {
  
-    const [showCategory, setShowCategory] = useState(false);
+  const [showCategory, setShowCategory] = useState(false);
    
-  const [saveCategory, setSaveCategory] = useState("");
+  //const [saveCategory, setSaveCategory] = useState("");
 
   const addToCategory = () => {
     setShowCategory((prevCategory) => !prevCategory);
@@ -13,34 +13,61 @@ const AddToCategory = ({ isLoggedIn, initialvalue, wishlist, closeCart }) => {
   };
   //category state
   const [categories, setCategories] = useState([]);
+  const[isEdit, setIsEdit] = useState({id: 1, status:false});
+  const Stateindex = -1;
   const [newCategory, setNewCategory] = useState('');
   const[successMsg, setSuccessMsg] = useState('');
 
-///console.log(newCategory);
+  const[curDropItem, setCurDropItem] = useState('');
 // add new category
   const saveToCategory = () => {
-   // console.log("save category");
+    let newId;
   if(newCategory){
-        let newId = categories.length + 1;
+        newId = categories.length + 1;
         let newCatEntry = {id : newId, title:newCategory, status:false}
         setCategories([...categories, newCatEntry])
         setNewCategory('');
         setSuccessMsg('Category saved successfully');
         setTimeout(() => {
          setSuccessMsg('');
-          
         }, 2000);
         setShowCategory(false)
     }
-   // console.log(categories)
+    localStorage.setItem('category_id',newId);
+    localStorage.setItem('category_name', newCategory)
   };
+
 // delete category
 const deleteCategory = (id) => {
   let newCategory = categories.filter(category => category.id !== id);
   setCategories(newCategory);
-  console.log(id)
 }
+
+//handling drop down item
+const dropDownHandler = (itemId) => {
+  console.log(itemId);
+  let curItem = categories.filter(category => category.id === itemId);
+  setCurDropItem(curItem[0].title)
+}
+
+const saveCat = (cat_id, index) => {
+  setIsEdit({ id: cat_id, status: !isEdit.status });
+  console.log('cat',cat_id,'edit',isEdit.id,index);
+}
+const editCat = (cat_id,index) => {
+  
+   setIsEdit({ id: cat_id, status: !isEdit.status });
+   console.log('cat',cat_id,'edit',isEdit.id,index);
+}
+
+//console.log(isEdit)
+const[catNameChange, setCatNameChange] = useState('');
+// const catNameChange = (e) => {
+//   console.log(e)
+// }
+const category_name = localStorage.getItem('category_name');
   return (
+    
     <>
       <Row className={isLoggedIn ? "title_container mb-2" : "title_container"}>
         <Col sm={1} style={{ marginTop: ".20rem" }}>
@@ -73,8 +100,7 @@ const deleteCategory = (id) => {
         )}
       </Row>
       {showCategory && (
-        <Form>
-          
+       
         <Row className="overlay">
           <Col sm={9} className="p-0">
             <input
@@ -95,8 +121,7 @@ const deleteCategory = (id) => {
             </Button>
           </Col>
         </Row>
-        </Form>
-        
+       
       )}
       {successMsg !== '' && (
         <ToastContainer position="top-start">
@@ -124,21 +149,21 @@ const deleteCategory = (id) => {
                     </Row>
                   </li>
                 </ul>
-                <ul className={categories.length === 0 && 'display-n'}>
+                <ul className={categories.length === 0 ? 'display-n':'display-b'}>
                 {categories && categories.map((category,index) => (
 
                   <li key={index}>
                     <Row>
                       <Col sm={10}>
                         {/* <span>{category.id} </span> */}
-                        <span>{category.title}</span>
+                       {!isEdit.status && category.id === isEdit.id ? (<span onClick={() => dropDownHandler(category.id)}>{category.title}</span>):
+                       (<input type='text' className='editCat' defaultValue={category_name?category_name:category.title}  onChange={(e) => setCatNameChange(e.target.value)}/>)
+                       }
                       </Col>
                       <Col sm={1}>
-                        <img
-                          alt="trash"
-                          className="trash-btn"
-                          src="https://s3.amazonaws.com/cdn.myshopapps.com/iwish/drawer/editCat.svg"
-                        />
+                        {!isEdit.status && category.id === isEdit.id ?
+                        <img alt="trash" className="trash-btn" onClick={()=>editCat(category.id,index)}  src="https://s3.amazonaws.com/cdn.myshopapps.com/iwish/drawer/editCat.svg" /> : <img alt="trash" className="trash-btn" onClick={()=>saveCat(category.id,index )}  src="https://s3.amazonaws.com/cdn.myshopapps.com/iwish/drawer/saveCat.svg" />}
+                        
                         {/*<img alt="trash" class="trash-btn" src="https://s3.amazonaws.com/cdn.myshopapps.com/iwish/drawer/saveCat.svg"/>*/}
                       </Col>
                       <Col sm={1} onClick={() => deleteCategory(category.id)}>
